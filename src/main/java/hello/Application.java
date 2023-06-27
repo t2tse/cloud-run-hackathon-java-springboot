78fb9d25d2a7674ec259d04737140f9ee940e064
tommytse <tommytse@google.com>
2023-06-27 15:57:06 +0800

    fix later

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -69,40 +70,55 @@ public class Application {
 
     System.out.println("myLocation["+myLocation.x+","+myLocation.y+"] Direction["+myLocation.direction+"] hit? "+myLocation.wasHit);
 
+    // get all other player locations
+    arenaUpdate.arena.state.remove(myURL);
+    Collection<PlayerState> otherLocations = arenaUpdate.arena.state.values();
 
-
-    // while(arenaUpdate.arena.state.values().iterator().hasNext()){
-    //   PlayerState playerState = arenaUpdate.arena.state.values().iterator().next();
-    //   if(playerState.){
-    //     return "HIT";
-    //   }
-    // }
-
-    // if(wasHit){
-    //   if(canMoveForward(arenaUpdate.arena)){
+    // TODO fix this later
+    // if(myLocation.wasHit){
+    //   if(canMoveForward(myLocation, otherLocations, width, height)){
 
     //     return "F";
     //   } else {
     //     // Cannot move forward, turn right
     //     return "R";
     //   }
+    // } 
 
-    // }
+    // not hit, attempt to hit someone or just move randomly
 
     // random command
     String[] commands = new String[]{"F", "R", "L", "T"};
     int i = new Random().nextInt(4);
     
     return commands[i];
+
   }
 
-  private boolean canMoveForward(Arena arena) {
+  private boolean canMoveForward(PlayerState myLocation, Collection<PlayerState> others, int width, int height) {
+    
+    int newX = myLocation.x;
+    int newY = myLocation.y;
+    
+    switch(myLocation.direction) {
+      case "E":
+        newX++;
+      case "S":      
+        newY++;
+      case "W":
+        newY--;
+      case "N":
+        newX--;
+    }
 
-    while(arena.state.values().iterator().hasNext()){
-      
+    for (PlayerState other : others) {
+      if (myLocation.x == other.x && myLocation.y == other.y) {
+      }
     }
     return false;
   }
 
+
 }
+
 
